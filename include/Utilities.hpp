0c264b33df58158fe197049c7cc3ec37fce7e7a3
Timon Emken <emken@chalmers.se>
2020-07-02 11:12:23 +0200

    Equip the progress bar with a simple remaining-time estimate

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -9,7 +9,7 @@ namespace libphysica
 {
 
 //1. Progress bar
-extern void Print_Progress_Bar(double progress, unsigned int MPI_rang = 0, unsigned int bar_length = 50);
+extern void Print_Progress_Bar(double progress, unsigned int MPI_rang = 0, unsigned int bar_length = 50, double time = 0.0);
 
 //2. Import and export data from files
 extern std::vector<double> Import_List(std::string filepath, double dimension = 1.0, unsigned int ignored_initial_lines = 0);
